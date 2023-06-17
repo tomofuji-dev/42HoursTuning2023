f08238dcfaecfb7f0974ce7f38263b280edda373
Yusuke Sato <yususato@c1r22s1.42tokyo.jp>
2023-06-17 16:28:54 +0900

    test2

--- app/mysql/mysql_migration/2_test.sql
+++ app/mysql/mysql_migration/2_test.sql
@@ -1,3 +1,12 @@
 CREATE INDEX idx_user_id ON user (user_id);
 
 CREATE INDEX idx_office_id ON user (office_id);
+
+SELECT user_id, user_name, office_id, user_icon_id
+FROM user
+WHERE user_id IN (
+    SELECT user_id
+    FROM user
+    ORDER BY RAND()
+    LIMIT N
+);
