106d59ba1a1b378944e7622d77df5db2908ed5ce
tfuji <tomofuji.dev@gmail.com>
2023-06-17 23:46:26 +0900

    goalのindexをとりあえず外した

--- app/mysql/mysql_migration/1_add_index.sql
+++ app/mysql/mysql_migration/1_add_index.sql
@@ -1,8 +1,9 @@
 -- userテーブル
 ALTER TABLE `user` ADD INDEX `idx_mail` (`mail`);
 ALTER TABLE `user` ADD INDEX `idx_user_name` (`user_name`);
+-- ALTER TABLE `user` ADD INDEX `idx_goal` (`goal`);
+ALTER TABLE `user` ADD INDEX `idx_entry_date` (`entry_date`);
 ALTER TABLE `user` ADD INDEX `idx_kana` (`kana`);
-ALTER TABLE `user` ADD INDEX `idx_mail` (`goal`);
 ALTER TABLE `user` ADD INDEX `idx_office_id` (`office_id`);
 ALTER TABLE `user` ADD INDEX `idx_user_icon_id` (`user_icon_id`);
 
