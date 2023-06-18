84f9aedad23cec1926cf45d3d82e87ea0d655b86
Yusuke Sato <yususato@c1r14s2.42tokyo.jp>
2023-06-18 13:31:09 +0900

    test

--- app/mysql/mysql_migration/2_test.sql
+++ app/mysql/mysql_migration/2_test.sql
@@ -7,15 +7,35 @@ CREATE INDEX idx_user_id ON `department_role_member` (`user_id`);
 CREATE INDEX idx_created_by ON `match_group` (`created_by`);
 CREATE INDEX idx_user_id ON `match_group_member` (`user_id`);
 
-CREATE TABLE `department_role_member` (
-    `department_role_member_id` INT NOT NULL AUTO_INCREMENT,
-    `department_id` VARCHAR(36) NOT NULL,
-    `role_id` VARCHAR(36) NOT NULL,
-    `user_id` VARCHAR(36) NOT NULL,
-    `entry_date` DATE NOT NULL,
-    `belong` TINYINT(1) NOT NULL DEFAULT '1',
-    PRIMARY KEY (`department_role_member_id`),
-    FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
-    FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-);
+max_connections = 200
+
+query_cache_type = 1
+query_cache_size = 256M
+
+innodb_buffer_pool_size = 1G
+key_buffer_size = 256M
+
+import redis
+import json
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def get_data_from_cache(key):
+    cached_data = r.get(key)
+    if cached_data:
+        return json.loads(cached_data)
+    else:
+        return None
+
+def save_data_to_cache(key, data):
+    r.set(key, json.dumps(data))
+
+def get_user_data(user_id):
+    cached_data = get_data_from_cache(f'user:{user_id}')
+    if cached_data:
+        return cached_data
+
+    db_data = execute_sql_query(f'SELECT * FROM user WHERE user_id = {user_id}')
+    save_data_to_cache(f'user:{user_id}', db_data)
+
+    return db_data
